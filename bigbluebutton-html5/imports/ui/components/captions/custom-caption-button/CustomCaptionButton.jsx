2a181c26a819e5e8dfddd3fef5658fbb7732d131
Imran Hossain <ahmedrakibimran@gmail.com>
2020-12-09 18:08:31 +0600

    test

--- bigbluebutton-html5/imports/ui/components/captions/custom-caption-button/CustomCaptionButton.jsx
+++ bigbluebutton-html5/imports/ui/components/captions/custom-caption-button/CustomCaptionButton.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ActionsBarService from '/imports/ui/components/actions-bar/service';
 import { withModalMounter } from '/imports/ui/components/modal/service';
 import CaptionsWriterMenu from '/imports/ui/components/captions/writer-menu/container';
+import Auth from '/imports/ui/services/auth';
+import Users from '/imports/api/users';
+import { makeCall } from '/imports/ui/services/api';
+
+const USER_CONFIG = Meteor.settings.public.user;
+const ROLE_MODERATOR = USER_CONFIG.role_moderator;
+const DIAL_IN_USER = 'dial-in-user';
 
 // const amIModerator = ActionsBarService.amIModerator();
 
@@ -11,11 +18,16 @@ function CustomCaptionButton(props) {
     mountModal(<CaptionsWriterMenu />);
   };
 
+  const amIPresenter = () => {
+    return Users.findOne({ userId: Auth.userID }, { fields: { role: 1 } });
+  };
+
   return (
     <>
       <button type="button" onClick={handleCaptionsClick}>
         Start CC
       </button>
+      {console.log(amIPresenter())}
     </>
   );
 }
